0ac9818ca5b106f096e0ce8f04755fb6c9ea94b0
superherointj <5861043+superherointj@users.noreply.github.com>
2021-10-05 11:43:12 -0300

    python39Packages.types-typed-ast: init 1.4.4
    Co-authored-by: @jnetod @veehaitch @nbraud

--- pkgs/development/python-modules/types-typed-ast/default.nix
+++ pkgs/development/python-modules/types-typed-ast/default.nix
@@ -5,25 +5,22 @@
 
 buildPythonPackage rec {
   pname = "types-typed-ast";
-  version = "1.5.1";
-  format = "setuptools";
+  version = "1.4.4";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-UQ876qlUkrNUTWfoFYGvopA8dktwiJ/82yhubGJn0pc=";
+    sha256 = "ffa0471e0ba19c4ea0cba0436d660871b5f5215854ea9ead3cb5b60f525af75a";
   };
 
   # Module doesn't have tests
   doCheck = false;
 
-  pythonImportsCheck = [
-    "typed_ast-stubs"
-  ];
+  pythonImportsCheck = [ "typed_ast-stubs" ];
 
   meta = with lib; {
     description = "Typing stubs for typed-ast";
     homepage = "https://github.com/python/typeshed";
     license = licenses.asl20;
-    maintainers = with maintainers; [ SuperSandro2000 veehaitch ];
+    maintainers = with maintainers; [ superherointj veehaitch ];
   };
 }
