68a6420171d3251a8fe3cddb85fdf46b06d7670b
midchildan <git@midchildan.org>
2021-11-27 01:51:57 +0900

    llvmPackages_13: build with llvmPackages_11 on Darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12794,6 +12794,8 @@ with pkgs;
     targetLlvmLibraries = targetPackages.llvmPackages_13.libraries or llvmPackages_13.libraries;
   } // lib.optionalAttrs (stdenv.hostPlatform.isi686 && buildPackages.stdenv.cc.isGNU) {
     stdenv = gcc7Stdenv;
+  } // lib.optionalAttrs stdenv.hostPlatform.isDarwin {
+    inherit (llvmPackages_11) stdenv;
   }));
 
   llvmPackages_latest = llvmPackages_13;
