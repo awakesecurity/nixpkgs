2ad84701d3a481931c534ec89131d086a0b7fc1a
Artemis Tosini <me@artem.ist>
2021-11-28 03:58:53 +0000

    rnix-lsp: Use nix 2.4
    In e6548105b7ffbcb20d0a091be358165a21b510e2 rnix-lsp switched back to
    using the default nix because it was moved to 2.4. However, in
    e3b7448f2301459365d3e6833b9321a6dbf1ae73 the default nix moved back to 2.3.16.
    As rnix-lsp requires at least nix 2.4 for tests to succeed, the tests
    started failing

--- pkgs/development/tools/rnix-lsp/default.nix
+++ pkgs/development/tools/rnix-lsp/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, rustPlatform, nix }:
+{ lib, fetchFromGitHub, rustPlatform, nix_2_4 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "rnix-lsp";
@@ -13,7 +13,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-71vH8oc8DmwbwM2PgxjGmWAbyC4AByx7waHxLsr2koI=";
 
-  checkInputs = [ nix ];
+  checkInputs = [ nix_2_4 ];
 
   meta = with lib; {
     description = "A work-in-progress language server for Nix, with syntax checking and basic completion";
