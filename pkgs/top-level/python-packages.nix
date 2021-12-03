37f027c012784dc009b8d9c8160a52eeeaa420a6
Samuel Ainsworth <skainsworth@gmail.com>
2021-12-02 17:02:28 -0800

    python3Packages.wandb: init at 0.12.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9914,6 +9914,8 @@ in {
 
   Wand = callPackage ../development/python-modules/Wand { };
 
+  wandb = callPackage ../development/python-modules/wandb { };
+
   warlock = callPackage ../development/python-modules/warlock { };
 
   warrant = callPackage ../development/python-modules/warrant { };
