56d537e4dab011235e1af360f049b64b8426c774
Frederik Rietdijk <fridh@fridh.nl>
2022-01-15 09:50:35 +0100

    python310: 3.10.1 -> 3.10.2
    (cherry picked from commit 8dabcce399d3a090679cd2180d6c30df99c97dc7)

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -129,6 +129,14 @@ with pkgs;
       };
       sha256 = "sha256-ZnZ6NTCdck83DfnlA8FytO5ET0nWK5i8TspyUSPibEk=";
     };
+    python310 = {
+      sourceVersion = {
+        major = "3";
+        minor = "10";
+        patch = "2";
+        suffix = "";
+      };
+      sha256 = "sha256-F946x9qfJRmqnWQ3jGA6c6DprVjf+ogS5FFgwIbeZMc=";
     };
   };
 
@@ -179,18 +187,11 @@ in {
     inherit passthruFun;
   } // sources.python39);
 
-  python310 = callPackage ./cpython {
+  python310 = callPackage ./cpython ({
     self = python310;
-    sourceVersion = {
-      major = "3";
-      minor = "10";
-      patch = "1";
-      suffix = "";
-    };
-    sha256 = "0xz1wrd6xi20sbli30vm6jclc4rlnnd03irybknf2p8sdrdjdwd7";
     inherit (darwin) configd;
     inherit passthruFun;
-  };
+  } // sources.python310);
 
   python311 = callPackage ./cpython {
     self = python311;
