40fec71348413c1daacaa4524a8ac10d5c31815a
Robert Scott <code@humanleg.org.uk>
2022-01-19 19:46:43 +0000

    wolfssl: add patch for CVE-2022-23408

--- pkgs/development/libraries/wolfssl/default.nix
+++ pkgs/development/libraries/wolfssl/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , autoreconfHook
 }:
 
@@ -14,6 +15,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}-stable";
     sha256 = "sha256-/noS5cn8lllWoGyZ9QyjRmdiR6LXzfT4lYGEt+0+Bdw=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-23408.patch";
+      url = "https://github.com/wolfSSL/wolfssl/commit/73b4cc9476f6355a91138f545f3fd007ce058255.patch";
+      sha256 = "0r3z6ybmx3ylnw9zdva3gq4jy691r471qvhy6dvdgmdksh2kx63v";
+    })
+  ];
 
   # Almost same as Debian but for now using --enable-all --enable-reproducible-build instead of --enable-distro to ensure options.h gets installed
   configureFlags = [
