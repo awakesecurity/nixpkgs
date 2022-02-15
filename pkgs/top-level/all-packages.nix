2ffec435edcd63252cd3ba1cde93101665988e97
Átila Saraiva <atilasaraiva@gmail.com>
2022-02-15 14:32:23 -0300

    oil-buku: init at 0.3.2
    (cherry picked from commit d3f07498cd422a4af4ddab9694ac88ccc3f9dac8)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3524,6 +3524,8 @@ with pkgs;
 
   odafileconverter = libsForQt5.callPackage ../applications/graphics/odafileconverter {};
 
+  oil-buku = callPackage ../applications/misc/oil { };
+
   ossutil = callPackage ../tools/admin/ossutil {};
 
   pastel = callPackage ../applications/misc/pastel {
