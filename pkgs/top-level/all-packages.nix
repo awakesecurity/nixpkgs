5802ac4e1ccf55069bb37c701928f43251444725
Átila Saraiva <atilasaraiva@gmail.com>
2022-02-15 23:02:35 -0300

    distrobox: init at 1.2.13
    (cherry picked from commit c2fc98a4aa14fbea8d4318094eafad889a3f86a9)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1689,6 +1689,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  distrobox = callPackage ../applications/virtualization/distrobox { };
+
   djmount = callPackage ../tools/filesystems/djmount { };
 
   dgsh = callPackage ../shells/dgsh { };
