c604b5c00e6f88ef4c07bd0ac026800e9487aee4
Sander van der Burg <svanderburg@gmail.com>
2022-03-13 14:31:43 +0100

    nixos/tomcat: add basic test case using the example app
    (cherry picked from commit 86fafe5f5026651ae5139f4880f177b350c8ec83)

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -475,6 +475,7 @@ in
   timezone = handleTest ./timezone.nix {};
   tinc = handleTest ./tinc {};
   tinydns = handleTest ./tinydns.nix {};
+  tomcat = handleTest ./tomcat.nix {};
   tor = handleTest ./tor.nix {};
   # traefik test relies on docker-containers
   traefik = handleTestOn ["x86_64-linux"] ./traefik.nix {};
