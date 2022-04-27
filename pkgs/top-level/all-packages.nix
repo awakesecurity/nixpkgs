40f5f3c24c0090e15760569ad356a5f6ea5dbdac
Vincent Laporte <Vincent.Laporte@gmail.com>
2022-04-27 11:35:20 +0200

    easycrypt-runtest: init at 2022.04
    (cherry picked from commit 1b6d9a36ebf7fe3150628ddb660ba75e95311078)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2919,6 +2919,8 @@ with pkgs;
 
   easycrypt = callPackage ../applications/science/logic/easycrypt { };
 
+  easycrypt-runtest = callPackage ../applications/science/logic/easycrypt/runtest.nix { };
+
   EBTKS = callPackage ../development/libraries/science/biology/EBTKS { };
 
   ecasound = callPackage ../applications/audio/ecasound { };
