f2e83582896bb60fa5e550f0d876b412b37ef726
phaer <hello@phaer.org>
2022-06-02 12:39:53 +0200

    python3Packages.cron-descriptor: init at 1.2.24

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1865,6 +1865,8 @@ in {
 
   criticality-score = callPackage ../development/python-modules/criticality-score { };
 
+  cron-descriptor = callPackage ../development/python-modules/cron-descriptor { };
+
   croniter = callPackage ../development/python-modules/croniter { };
 
   crownstone-cloud = callPackage ../development/python-modules/crownstone-cloud { };
