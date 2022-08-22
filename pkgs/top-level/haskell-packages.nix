7083db7ea8c266e9f551a0dd38014ac3e1cd6ccf
sternenseemann <sternenseemann@systemli.org>
2022-08-22 21:12:30 +0200

    haskell.compiler: ghc941 -> ghc942

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -17,14 +17,14 @@ let
     "ghc902"
     "ghc924"
     "ghc928"
-    "ghc941"
+    "ghc942"
     "ghcHEAD"
   ];
 
   nativeBignumIncludes = [
     "ghc902"
     "ghc924"
-    "ghc941"
+    "ghc942"
     "ghcHEAD"
   ];
 
@@ -156,7 +156,7 @@ in {
       buildTargetLlvmPackages = pkgsBuildTarget.llvmPackages_12;
       llvmPackages = pkgs.llvmPackages_12;
     };
-    ghc941 = callPackage ../development/compilers/ghc/9.4.1.nix {
+    ghc942 = callPackage ../development/compilers/ghc/9.4.2.nix {
       bootPkgs =
         # Building with 9.2 is broken due to
         # https://gitlab.haskell.org/ghc/ghc/-/issues/21914
@@ -307,9 +307,9 @@ in {
       ghc = bh.compiler.ghc928;
       compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.2.x.nix { };
     };
-    ghc941 = callPackage ../development/haskell-modules {
-      buildHaskellPackages = bh.packages.ghc941;
-      ghc = bh.compiler.ghc941;
+    ghc942 = callPackage ../development/haskell-modules {
+      buildHaskellPackages = bh.packages.ghc942;
+      ghc = bh.compiler.ghc942;
       compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.4.x.nix { };
     };
     ghcHEAD = callPackage ../development/haskell-modules {
