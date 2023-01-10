9ca0af4eb059a7fbca09cd42ae70db24c787f5b9
Åsmund Østvold <asmundo@gmail.com>
2023-01-10 20:58:20 +0100

    waagent: update to 2.8.0.11 (#206974)
    The agent has not been updated for a very long time.  In addition to
    updating to the newest tagged version the change creates a package for
    it.
    
    The existing version has issues with the new python2.7 package not
    containing crypt.so file. And the commit
    6910a4eea0038728a2f10ce84122806f2cb6b170 I believe introduced
    regression that caused the shebang to not be updated.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16405,6 +16405,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security SystemConfiguration;
   };
 
+  waagent = callPackage ../applications/networking/cluster/waagent { };
+
   wasm = ocamlPackages.wasm;
 
   wasm3 = callPackage ../development/interpreters/wasm3 { };
