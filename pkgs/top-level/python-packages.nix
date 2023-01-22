1329d0ed731fbb914b865ac0fec755d3f2c04b8c
Jesse Moore <jesse@jessemoore.dev>
2023-01-21 23:55:04 -0600

    python3Packages.returns: init at 0.19.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9300,6 +9300,8 @@ in {
 
   retrying = callPackage ../development/python-modules/retrying { };
 
+  returns = callPackage ../development/python-modules/returns { };
+
   retworkx = callPackage ../development/python-modules/retworkx { };
 
   rfc3339 = callPackage ../development/python-modules/rfc3339 { };
