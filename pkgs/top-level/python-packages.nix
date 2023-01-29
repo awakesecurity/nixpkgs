cac0ef4913e590e1b33878ef97329391f662a0e2
Matt Melling <mattmelling@fastmail.com>
2023-01-29 21:29:28 +0000

    python3Packages.sshfs: init at 2023.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10995,6 +10995,8 @@ self: super: with self; {
 
   ssh-mitm = callPackage ../development/python-modules/ssh-mitm { };
 
+  sshfs = callPackage ../development/python-modules/sshfs { };
+
   sshpubkeys = callPackage ../development/python-modules/sshpubkeys { };
 
   sshtunnel = callPackage ../development/python-modules/sshtunnel { };
