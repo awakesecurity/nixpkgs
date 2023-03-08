c374b24dce16f5296682a61fd1b61c58906ec036
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-03-09 00:31:39 +0100

    python310Packages.quart: init at 0.18.3
    An async Python micro framework for building web applications.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9064,6 +9064,8 @@ in {
 
   quantum-gateway = callPackage ../development/python-modules/quantum-gateway { };
 
+  quart = callPackage ../development/python-modules/quart { };
+
   querystring_parser = callPackage ../development/python-modules/querystring-parser { };
 
   questionary = callPackage ../development/python-modules/questionary { };
