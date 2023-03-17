6331f1e093a11859becf704f9d9880780d2d0eaa
sternenseemann <sternenseemann@systemli.org>
2023-03-17 16:06:20 +0100

    haskellPackages.hspec*: reflect 2.10.9 -> 2.10.10

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -746,12 +746,13 @@ self: super: {
   sensei = overrideCabal (drv: {
     testHaskellDepends = drv.testHaskellDepends or [] ++ [ self.hspec-meta_2_10_5 ];
     testToolDepends = drv.testToolDepends or [] ++ [ pkgs.git ];
-  }) (super.sensei.override {
-    hspec = self.hspec_2_10_9;
-    hspec-wai = super.hspec-wai.override {
-      hspec = self.hspec_2_10_9;
-    };
-  });
+  }) (super.sensei.overrideScope (self: super: {
+    hspec-meta = self.hspec-meta_2_10_10;
+    hspec = self.hspec_2_10_10;
+    hspec-core = dontCheck self.hspec-core_2_10_10;
+    hspec-discover = self.hspec-discover_2_10_10;
+    shelly = dontCheck super.shelly; # disable checks, because the newer hspec in this overrideScope doesn‘t work with newest hspec-contrib
+  }));
 
   # Depends on broken fluid.
   fluid-idl-http-client = markBroken super.fluid-idl-http-client;
@@ -1436,16 +1437,16 @@ self: super: {
   servant-openapi3 = dontCheck super.servant-openapi3;
 
   # Give hspec 2.10.* correct dependency versions without overrideScope
-  hspec_2_10_9 = doDistribute (super.hspec_2_10_9.override {
-    hspec-discover = self.hspec-discover_2_10_9;
-    hspec-core = self.hspec-core_2_10_9;
+  hspec_2_10_10 = doDistribute (super.hspec_2_10_10.override {
+    hspec-discover = self.hspec-discover_2_10_10;
+    hspec-core = self.hspec-core_2_10_10;
   });
-  hspec-discover_2_10_9 = super.hspec-discover_2_10_9.override {
+  hspec-discover_2_10_10 = doDistribute (super.hspec-discover_2_10_10.override {
     hspec-meta = self.hspec-meta_2_10_5;
-  };
-  hspec-core_2_10_9 = super.hspec-core_2_10_9.override {
+  });
+  hspec-core_2_10_10 = doDistribute (super.hspec-core_2_10_10.override {
     hspec-meta = self.hspec-meta_2_10_5;
-  };
+  });
 
   # Point hspec 2.7.10 to correct dependencies
   hspec_2_7_10 = super.hspec_2_7_10.override {
