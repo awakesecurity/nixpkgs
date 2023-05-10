218b606e3dabac79df95c98c3b3e412d58782508
John Soo <john.soo@arista.com>
2023-05-10 11:36:04 -0700

    python310Packages.wasabi: 0.9.0 -> 0.9.1
    # Conflicts:
    #	pkgs/development/python-modules/wasabi/default.nix

--- pkgs/development/python-modules/wasabi/default.nix
+++ pkgs/development/python-modules/wasabi/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "wasabi";
-  version = "0.8.2";
+  version = "0.9.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b4a36aaa9ca3a151f0c558f269d442afbb3526f0160fd541acd8a0d5e5712054";
+    sha256 = "sha256-rabxPptw7ya/lfrQ/r396+IAXimgitWPS7rjg6lymM8=";
   };
 
   checkInputs = [ pytestCheckHook ];
