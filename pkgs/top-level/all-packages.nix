919bbd23bcea96f72b252ab2f5fc9a4b681ca436
Tomas Drtina <tdrtina-ext@arista.com>
2023-05-11 17:58:39 +0200

    modern-cpp-kafka: init at 2023.03.07

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10984,6 +10984,8 @@ with pkgs;
 
   mkgmap-splitter = callPackage ../applications/misc/mkgmap/splitter { };
 
+  modern-cpp-kafka = callPackage ../development/libraries/modern-cpp-kafka {};
+
   mtm = callPackage ../tools/misc/mtm { };
 
   op-geth = callPackage ../applications/blockchains/optimism/geth.nix {
