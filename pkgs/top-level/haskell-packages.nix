b9a5e90a793e9bbab604fa775c3adfad3693aa00
Dennis Gosnell <cdep.illabout@gmail.com>
2023-05-27 17:18:10 +0900

    haskell.compiler.ghc928: init at 9.2.8

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -16,6 +16,7 @@ let
     "native-bignum"
     "ghc902"
     "ghc924"
+    "ghc928"
     "ghc941"
     "ghcHEAD"
   ];
@@ -161,6 +162,16 @@ in {
         # Need to use 902 due to
         # https://gitlab.haskell.org/ghc/ghc/-/issues/21914
           packages.ghc902;
+    };
+    ghc928 = callPackage ../development/compilers/ghc/9.2.8.nix {
+      bootPkgs =
+        # aarch64 ghc8107Binary exceeds max output size on hydra
+        if stdenv.hostPlatform.isAarch then
+          packages.ghc8107BinaryMinimal
+        else if stdenv.hostPlatform.isPower64 && stdenv.hostPlatform.isLittleEndian then
+          packages.ghc810
+        else
+          packages.ghc8107Binary;
       inherit (buildPackages.python3Packages) sphinx;
       # Need to use apple's patched xattr until
       # https://github.com/xattr/xattr/issues/44 and
@@ -280,6 +291,11 @@ in {
     ghc924 = callPackage ../development/haskell-modules {
       buildHaskellPackages = bh.packages.ghc924;
       ghc = bh.compiler.ghc924;
+      compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.2.x.nix { };
+    };
+    ghc928 = callPackage ../development/haskell-modules {
+      buildHaskellPackages = bh.packages.ghc928;
+      ghc = bh.compiler.ghc928;
       compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.2.x.nix { };
     };
     ghc941 = callPackage ../development/haskell-modules {
