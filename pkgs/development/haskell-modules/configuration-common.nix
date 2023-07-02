c052f1102c377361e08bf1519482c6b8a4ec2d53
sternenseemann <sternenseemann@systemli.org>
2023-07-02 14:16:59 +0200

    haskell.packages.*.cabal-install: provide correct versions of deps
    Some packages cabal-install needed a certain version range (cabal-install-solver
    and resolv) have versions incompatible with these in Stackage LTS 21. We can,
    however, easily provide the necessary ones.
    
    Additionally, we clean up some old resolv related overrides that are no longer
    necessary or we no longer care about (being able to build cabal-install with
    GHC 8.6, namely).

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -34,7 +34,12 @@ self: super: {
     let
       # !!! Use cself/csuper inside for the actual overrides
       cabalInstallOverlay = cself: csuper:
-        lib.optionalAttrs (lib.versionOlder self.ghc.version "9.6") {
+        {
+          # Needs to be upgraded compared to Stackage LTS 21
+          cabal-install-solver = cself.cabal-install-solver_3_10_1_0;
+          # Needs to be downgraded compared to Stackage LTS 21
+          resolv = cself.resolv_0_1_2_0;
+        } // lib.optionalAttrs (lib.versionOlder self.ghc.version "9.6") {
           Cabal = cself.Cabal_3_10_1_0;
           Cabal-syntax = cself.Cabal-syntax_3_10_1_0;
         } // lib.optionalAttrs (lib.versionOlder self.ghc.version "9.4") {
@@ -291,7 +296,7 @@ self: super: {
 
   # Tests require older versions of tasty.
   hzk = dontCheck super.hzk;
-  resolv = doJailbreak super.resolv;
+  resolv_0_1_2_0 = doJailbreak super.resolv_0_1_2_0;
 
   # Tests require a Kafka broker running locally
   haskakafka = dontCheck super.haskakafka;
@@ -1057,10 +1062,6 @@ self: super: {
       cp -v *.info* $out/share/info/
     '';
   }) super.hledger-web;
-
-
-  # https://github.com/haskell-hvr/resolv/pull/6
-  resolv_0_1_1_2 = dontCheck super.resolv_0_1_1_2;
 
   # The test suite does not know how to find the 'alex' binary.
   alex = overrideCabal (drv: {
