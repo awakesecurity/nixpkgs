9d88cde109b6de90692368ccb3eb6f8985a4c21d
sternenseemann <sternenseemann@systemli.org>
2023-07-02 14:16:59 +0200

    haskell.packages.*.cabal-install: provide correct versions of deps
    Some packages cabal-install needed a certain version range (cabal-install-solver
    and resolv) have versions incompatible with these in Stackage LTS 21. We can,
    however, easily provide the necessary ones.
    
    Additionally, we clean up some old resolv related overrides that are no longer
    necessary or we no longer care about (being able to build cabal-install with
    GHC 8.6, namely).

--- pkgs/development/haskell-modules/configuration-ghc-9.0.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.0.x.nix
@@ -80,7 +80,6 @@ self: super: {
   parallel = doJailbreak super.parallel;
   primitive = doJailbreak (dontCheck super.primitive);
   regex-posix = doJailbreak super.regex-posix;
-  resolv = doJailbreak super.resolv;
   singleton-bool = doJailbreak super.singleton-bool;
   split = doJailbreak super.split;
   tar = doJailbreak super.tar;
