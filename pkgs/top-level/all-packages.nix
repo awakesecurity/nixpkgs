a1125f528fe1b67e6dd36f62df82509e5c214d4d
John Soo <john.soo@arista.com>
2023-07-13 09:06:10 -0700

    drop! makeWrapper: Drop test that doesn't exist.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -783,7 +783,6 @@ with pkgs;
     { deps = [ dieHook ];
       substitutions = {
         shell = targetPackages.runtimeShell;
-        passthru.tests = tests.makeWrapper;
       };
     }
     ../build-support/setup-hooks/make-wrapper.sh;
