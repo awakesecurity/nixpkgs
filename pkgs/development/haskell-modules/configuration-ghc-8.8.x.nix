aa3d5ccf0555ffe8e125a5a80989dfd4f7be2505
sternenseemann <sternenseemann@systemli.org>
2023-07-25 13:01:20 +0200

    haskell.packages.*.ghc-source-gen: shuffle broken flags around
    ghc-source-gen being broken is the norm now, as it only supports
    GHC < 9.4. To keep tabs on it still (it is required for HLS some of the
    time), we add it to release-haskell.nix.

--- pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.8.x.nix
@@ -174,4 +174,21 @@ self: super: {
 
   # Depends on OneTuple for GHC < 9.0
   universe-base = addBuildDepends [ self.OneTuple ] super.universe-base;
+
+  # doctest-parallel dependency requires newer Cabal
+  regex-tdfa = dontCheck super.regex-tdfa;
+
+  # Unnecessarily strict lower bound on base
+  # https://github.com/mrkkrp/megaparsec/pull/485#issuecomment-1250051823
+  megaparsec = doJailbreak super.megaparsec;
+
+  # Needs OneTuple for ghc < 9.2
+  binary-orphans = addBuildDepends [ self.OneTuple ] super.binary-orphans;
+
+  # Later versions only support GHC >= 9.2
+  ghc-exactprint = self.ghc-exactprint_0_6_4;
+  apply-refact = self.apply-refact_0_9_3_0;
+
+  # Requires GHC < 9.4
+  ghc-source-gen = doDistribute (unmarkBroken super.ghc-source-gen);
 }
