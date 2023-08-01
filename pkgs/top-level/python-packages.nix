87479e84679575950a7277d8668dda385ee792ee
Arunabh <arunabhghosh98@gmail.com>
2023-08-01 14:16:38 -0700

    add the pymisp package to the list of python packages.
    Co-Authored-By: John Soo <jsoo1@users.noreply.github.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8217,6 +8217,8 @@ self: super: with self; {
 
   pymfy = callPackage ../development/python-modules/pymfy { };
 
+  pymisp = callPackage ../development/python-modules/pymisp { };
+
   pymodbus = callPackage ../development/python-modules/pymodbus { };
 
   pymongo = callPackage ../development/python-modules/pymongo { };
