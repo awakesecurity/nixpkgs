83f58fe1adfc2b674342c680682caa22c6d24b1e
John Soo <john.soo@arista.com>
2023-08-30 09:47:13 -0700

    mailsend-go: init at 1.0.10
    mailsend-go is a rewrite of mailsend in go.
    
    mailsend is not receiving feature updates as of 2019-02-11.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2822,6 +2822,8 @@ with pkgs;
 
   madlang = haskell.lib.compose.justStaticExecutables haskellPackages.madlang;
 
+  mailsend-go = callPackage ../tools/networking/mailsend-go { };
+
   man = man-db;
 
   marimo = with python3Packages; toPythonApplication marimo;
