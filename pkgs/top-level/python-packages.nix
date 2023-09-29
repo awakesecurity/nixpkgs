745767ecb63364d46f91e43f336b1d9eeb60259d
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-09-29 06:05:51 -0700

    python3Packages.treelib: init at 1.7.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10800,6 +10800,8 @@ in {
 
   trectools = callPackage ../development/python-modules/trectools { };
 
+  treelib = callPackage ../development/python-modules/treelib { };
+
   treelog = callPackage ../development/python-modules/treelog { };
 
   treeo = callPackage ../development/python-modules/treeo { };
