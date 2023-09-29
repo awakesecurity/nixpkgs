0d92c11d9e2132042d4a1d118da0ef2b3e6a3773
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-09-29 07:21:58 -0700

    python3Packages.wikipedia-api: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12032,6 +12032,8 @@ self: super: with self; {
 
   wikipedia = callPackage ../development/python-modules/wikipedia { };
 
+  wikipedia-api = callPackage ../development/python-modules/wikipedia-api { };
+
   willow = callPackage ../development/python-modules/willow { };
 
   winacl = callPackage ../development/python-modules/winacl { };
