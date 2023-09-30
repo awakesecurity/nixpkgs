3e50fe47ba56a9bee33deb1ff036bfd33df03b29
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-09-30 14:44:41 -0700

    python3Packages.hyperscan: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4189,6 +4189,8 @@ in {
 
   hyperframe = callPackage ../development/python-modules/hyperframe { };
 
+  hyperscan = callPackage ../development/python-modules/hyperscan { };
+
   hyperion-py = callPackage ../development/python-modules/hyperion-py { };
 
   hyperlink = callPackage ../development/python-modules/hyperlink { };
