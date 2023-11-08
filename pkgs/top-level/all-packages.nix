632531bb5537be8c153258eee403b9d50de58d58
Parnell Springmeyer <parnell@arista.com>
2023-11-08 11:30:05 -0600

    terraform-cloud-agent: init at 1.13.0
    This change adds a derivation for the [`terraform-cloud-agent`][1]
    executable and a NixOS module that defines a systemd service for
    running that agent.
    
    [1]: https://developer.hashicorp.com/terraform/cloud-docs/agents.
    
    `stripRoot = false`
    
    Specify the hash
    
    Install tfc-agent binaries to `$out/bin`
    
    Declare the `mainProgram` metadata for this package
    
    ... so that it can be used with `lib.getExe`.
    
    Use `lib.getExe`
    
    Suggested by @jsoo1.
    
    Use freeformType for NixOS options that are turned into flags
    
    Suggested by @jsoo1.
    
    No need to guard on whether `cache-dir` and `data-dir` are null
    
    Suggested by @jsoo1.
    
    Ensure `/etc/terraform-cloud-agent/`
    
    Suggested by @jsoo1.
    
    Use the systemd-configured configuration directory
    
    Use the standard systemd environment identifiers `%S` and `%C`
    
    Suggested by @jsoo1.
    
    Add `user` and `group` and drop privileges
    
    Rider: add sandoxing arguments cribbed from the nginx module.
    
    Just give the `flags` attrset as an argument
    
    Sequence after `network-online.target` instead
    
    ... since that's what we really mean.
    
    Add doc strings and examples

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14574,6 +14574,8 @@ with pkgs;
     callPackage ../applications/networking/cluster/terraform-providers { }
   );
 
+  terraform-cloud-agent = callPackage ../tools/infrastructure/terraform-cloud-agent { };
+
   vaultenv = haskell.lib.justStaticExecutables haskellPackages.vaultenv;
 
   vaultwarden-sqlite = vaultwarden;
