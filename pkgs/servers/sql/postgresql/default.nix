1cd759a488af2e9da69eefa1dd9f6db6f2969b8a
John Soo <john.soo@arista.com>
2023-12-21 22:34:47 -0800

    drop! postgresql_9: sha256 -> hash
    But postgresql 9 is unsupported, just kept alive for no reason.

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -209,7 +209,7 @@ in self: {
   postgresql_9_6 = self.callPackage generic {
     version = "9.6.24";
     psqlSchema = "9.6";
-    sha256 = "sha256-rrehlr4+vtGnR271ZfOXIhh8EI3UfadIm+nE/K6YKs4=";
+    hash = "sha256-rrehlr4+vtGnR271ZfOXIhh8EI3UfadIm+nE/K6YKs4=";
     this = self.postgresql_9_6;
     thisAttr = "postgresql_9_6";
     inherit self;
