ee771f4693879bdab8ab9f8d9eab07ba5a7ad0be
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-24 15:53:49 +0100

    python311Packages.types-psycopg2: init at 2.9.21.20

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12917,6 +12917,8 @@ self: super: with self; {
 
   types-psutil = callPackage ../development/python-modules/types-psutil { };
 
+  types-psycopg2 = callPackage ../development/python-modules/types-psycopg2 { };
+
   types-pyopenssl = callPackage ../development/python-modules/types-pyopenssl { };
 
   types-python-dateutil = callPackage ../development/python-modules/types-python-dateutil { };
