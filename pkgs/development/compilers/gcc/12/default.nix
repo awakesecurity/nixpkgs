5b0580358805e935618f5120ebab59610d7821e5
Tomas Drtina <tdrtina-ext@arista.com>
2024-06-24 18:02:36 +0200

    gcc12: patch issue 109504 - f16 without SSE2
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=109504
    https://gcc.gnu.org/git/gitweb.cgi?p=gcc.git;h=9a19fa8b616f83474c35cc5b34a3865073ced829

--- pkgs/development/compilers/gcc/12/default.nix
+++ pkgs/development/compilers/gcc/12/default.nix
@@ -64,6 +64,7 @@ let majorVersion = "12";
         ../gnat-cflags-11.patch
         ../gcc-12-gfortran-driving.patch
         ../ppc-musl.patch
+        ./gcc-issue-109504-float16.patch
       ]
       # We only apply this patch when building a native toolchain for aarch64-darwin, as it breaks building
       # a foreign one: https://github.com/iains/gcc-12-branch/issues/18
