c823686bb722ae571162043e2a0f644d85b44443
Tomas Drtina <tdrtina-ext@arista.com>
2024-10-27 16:50:58 +0100

    haskellPackages.hw-json-simd: fix build for Darwin/clang

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -3104,8 +3104,9 @@ self: super: {
   }) super.proto3-wire;
 
   # c2hs fails to build intrinsics from gcc{12,13,14}
-  hw-json-simd = lib.pipe super.hw-json-simd ([
-    unmarkBroken
-  ] ++ (lib.optional pkgs.stdenv.cc.isGNU (appendConfigureFlags [ "--with-gcc=${pkgs.gcc11}/bin/gcc" ])));
-
+  hw-json-simd = lib.pipe super.hw-json-simd (
+    [ unmarkBroken ]
+    ++ (lib.optional pkgs.stdenv.cc.isGNU (appendConfigureFlags [ "--with-gcc=${pkgs.gcc11}/bin/gcc" ]))
+    ++ (lib.optional pkgs.stdenv.cc.isClang (appendConfigureFlags [ "--with-gcc=${self.ghc.llvmPackages.clang}/bin/clang" ]))
+  );
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
