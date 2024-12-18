6c4399a7722841451ccb5f92da4f4ee19bcd520f
Tomas Drtina <tdrtina-ext@arista.com>
2024-12-18 15:25:04 +0100

    Revert "postgresql16Packages.pg_partman: 4.7.4 -> 5.0.0"
    This reverts commit daaca707452a4290ba76c39f352408b0ec35d9c7.

--- pkgs/servers/sql/postgresql/ext/pg_partman.nix
+++ pkgs/servers/sql/postgresql/ext/pg_partman.nix
@@ -2,19 +2,19 @@
 
 buildPostgresqlExtension rec {
   pname = "pg_partman";
-  version = "5.0.0";
+  version = "4.7.4";
 
   src = fetchFromGitHub {
     owner  = "pgpartman";
     repo   = pname;
     rev    = "refs/tags/v${version}";
-    sha256 = "sha256-T7+cPi8LIftWVwI9mi0LAwWCTxp/r6iyKT1wKO/Ztbk=";
+    sha256 = "sha256-DdE/hqCrju678Xk3xXGVFhKQM3x9skQQKolNJ2/3gbs=";
   };
 
   meta = with lib; {
     description = "Partition management extension for PostgreSQL";
     homepage    = "https://github.com/pgpartman/pg_partman";
-    changelog   = "https://github.com/pgpartman/pg_partman/blob/v${version}/CHANGELOG.md";
+    changelog   = "https://github.com/pgpartman/pg_partman/raw/v${version}/CHANGELOG.txt";
     maintainers = with maintainers; [ ggpeti ];
     platforms   = postgresql.meta.platforms;
     license     = licenses.postgresql;
