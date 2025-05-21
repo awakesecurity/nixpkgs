35e938851c1ba9f2b5f22dfd26939fed36d8589f
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2025-05-21 02:22:33 -0700

    Reapply "postgresql16Packages.pg_partman: 4.7.4 -> 5.0.0"
    This reverts commit 6c4399a7722841451ccb5f92da4f4ee19bcd520f.

--- pkgs/servers/sql/postgresql/ext/pg_partman.nix
+++ pkgs/servers/sql/postgresql/ext/pg_partman.nix
@@ -2,19 +2,19 @@
 
 buildPostgresqlExtension rec {
   pname = "pg_partman";
-  version = "4.7.4";
+  version = "5.0.0";
 
   src = fetchFromGitHub {
     owner  = "pgpartman";
     repo   = pname;
     rev    = "refs/tags/v${version}";
-    sha256 = "sha256-DdE/hqCrju678Xk3xXGVFhKQM3x9skQQKolNJ2/3gbs=";
+    sha256 = "sha256-T7+cPi8LIftWVwI9mi0LAwWCTxp/r6iyKT1wKO/Ztbk=";
   };
 
   meta = with lib; {
     description = "Partition management extension for PostgreSQL";
     homepage    = "https://github.com/pgpartman/pg_partman";
-    changelog   = "https://github.com/pgpartman/pg_partman/raw/v${version}/CHANGELOG.txt";
+    changelog   = "https://github.com/pgpartman/pg_partman/blob/v${version}/CHANGELOG.md";
     maintainers = with maintainers; [ ggpeti ];
     platforms   = postgresql.meta.platforms;
     license     = licenses.postgresql;
