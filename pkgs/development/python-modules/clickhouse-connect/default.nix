663ace264669143993375370ca57eb1f76ea1ccf
Tomas Drtina <tdrtina-ext@arista.com>
2026-03-02 10:40:30 -0800

    python3Package.clickhouse-connect: disable broken test

--- pkgs/development/python-modules/clickhouse-connect/default.nix
+++ pkgs/development/python-modules/clickhouse-connect/default.nix
@@ -52,11 +52,13 @@ buildPythonPackage rec {
     pytest-dotenv
   ]
   ++ optional-dependencies.sqlalchemy
-  ++ optional-dependencies.numpy;
+  ++ optional-dependencies.numpy
+  ++ optional-dependencies.pandas;
 
   # these tests require a running clickhouse instance
   disabledTestPaths = [
     "tests/integration_tests"
+    "tests/unit_tests/test_driver/test_httpclient.py"
   ];
 
   pythonImportsCheck = [
