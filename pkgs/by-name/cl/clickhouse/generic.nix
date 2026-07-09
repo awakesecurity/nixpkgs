8dfdbe1cb0c165ee3fc0f1c95918e8f3c11f342d
Tomas Drtina <tdrtina-ext@arista.com>
2026-07-09 09:13:14 -0700

    Revert "clickhouse: Move `binutils` to nativeBuildInputs"
    This reverts commit 5c6616e99b3e56d76d7187df5416e88999c181ad.

--- pkgs/by-name/cl/clickhouse/generic.nix
+++ pkgs/by-name/cl/clickhouse/generic.nix
@@ -104,10 +104,6 @@ llvmStdenv.mkDerivation (finalAttrs: {
     python3
     perl
     llvmPackages.lld
-    # Provides llvm-ar/llvm-objcopy.
-    # Required by cmake/strip_rust_symbols.sh to match the LLVM toolchain
-    # Otherwise it corrupts .eh_frame in the Rust staticlibs
-    llvmPackages.bintools
     removeReferencesTo
     zstd
   ]
@@ -116,6 +112,7 @@ llvmStdenv.mkDerivation (finalAttrs: {
     yasm
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    llvmPackages.bintools
     findutils
     darwin.bootstrap_cmds
   ]
