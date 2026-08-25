61b5df06bfe99a1f2fd9cccd3b22131c5d3f72da
Zoe Zuser <mel.zuser@arista.com>
2026-08-25 19:13:01 +0000

    openssh: 10.3p1 -> 10.4p1 (#297)
    addresses: CVE-2026-59995, CVE-2026-59996, CVE-2026-60001, and
    CVE-2026-60002
    
    ---------
    
    Co-authored-by: Morgan Jones <me@numin.it>

--- pkgs/tools/networking/openssh/default.nix
+++ pkgs/tools/networking/openssh/default.nix
@@ -14,11 +14,11 @@ in
 {
   openssh = common rec {
     pname = "openssh";
-    version = "10.2p1";
+    version = "10.4p1";
 
     src = fetchurl {
       url = urlFor version;
-      hash = "sha256-zMQsBBmTeVkmP6Hb0W2vwYxWuYTANWLSk3zlamD3mLI=";
+      hash = "sha256-72Am3SrqjVYFljjV0yYpAsiSzrqfiDlYNeDQbT+2Mjg=";
     };
 
     extraPatches = [
